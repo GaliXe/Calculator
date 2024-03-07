e9ca4be752b23841ad63245b488fa6e69d69d671
nkaspar3 <nkaspare@emich.edu>
2024-03-07 11:01:42 -0500

    added basic addition

--- src/CalculatorDriver.java
+++ src/CalculatorDriver.java
@@ -2,6 +2,5 @@ public class CalculatorDriver {
     static Expressions Exp = new Expressions();
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Exp.add();
     }
 }
--- src/Expressions.java
+++ src/Expressions.java
@@ -3,13 +3,31 @@ import java.util.Scanner;
 public class Expressions {
     Scanner input = new Scanner(System.in);
     boolean cont = true;
+    double result;
 
     public void add()
     {
         double int1 = input.nextInt();
         double int2 = input.nextInt();
-        double result = int1 + int2;
+        result = int1 + int2;
         
         System.out.print(result);
+
+        while(cont = true)
+        {
+            System.out.println("Wish to continue with?: ");
+            String contOp = input.next();
+            
+            switch(contOp)
+            {
+                case "yes":
+                break;
+
+                case "no": cont = false;
+                break;
+
+                default: System.out.println("INVALID CHOICE");
+            }    
+        }
     }
 }
