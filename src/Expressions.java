160440ccc1fb16d166d687ecb675ba4bba142e59
Nicholas Kasparek <nkaspare@emich.edu>
2024-03-11 16:26:16 -0400

    subtraction implementation started

--- src/Expressions.java
+++ src/Expressions.java
@@ -5,6 +5,7 @@ public class Expressions {
     boolean cont = false;
     double result;
 
+    //Addition
     public void add()
     {
         double int1 = input.nextInt();
@@ -39,8 +40,38 @@ public class Expressions {
         System.out.println(result);
     }
 
+`   //Subtraction
     public void subtract()
     {
+        double int1 = input.nextInt();
+        double int2 = input.nextInt();
+        result = int1 - int2;
+        
+        System.out.println(result);
 
+        while(!cont)
+        {
+            System.out.println("Wish to continue with?: ");
+            String contOp = input.next();
+            
+            switch(contOp)
+            {
+                case "yes": contSubtract();
+                break;
+
+                case "no": cont = true;
+                break;
+
+                default: System.out.println("INVALID CHOICE");
+            }    
+        }
+        System.out.println("Exiting expression"); 
+    }
+
+    public void contSubtract()
+    {
+        double intAdditional = input.nextInt();
+        result = result + intAdditional;
+        System.out.println(result);
     }
 }
